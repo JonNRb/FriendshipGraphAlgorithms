4179e9c94387d2e1eba10ec953e42d2abd1308ae
JonNRb <jonbetti@gmail.com>
2015-04-22 15:31:35 -0400

    Adding Heap class. Not done yet.

--- Graph.java
+++ Graph.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.lang.*;
 
 public class Graph {
 
@@ -37,8 +38,57 @@ public class Graph {
 	}
 
 	public static class PersonNotFoundException extends Exception {
+		public PersonNotFoundException() {
+			super();
+		}
 		public PersonNotFoundException(String s) {
 			super(s);
+		}
+		public PersonNotFoundException(Person p) {
+			if (p != null)
+				super("Person \"" + p.name + "\" that goes to \"" + p.school + "\" not found.");
+			else
+				super();
+		}
+	}
+
+	private class MinHeap<S,T implements Comparable><{
+		private class HeapNode<S,T implements Comparable> {
+			S data;
+			T weight;
+
+			public HeapNode<S,T>(S data, T weight) {
+				this.data = data;
+				this.weight = weight;
+			}
+		}
+
+		private ArrayList<HeapNode<S,T>> heap;
+
+		public MinHeap<S,T>() {
+			heap = new ArrayList<>();
+		}
+
+		public void insertNode(S data, T weight) {
+			HeapNode newNode = new HeapNode(data, weight);
+			heap.add(newNode);
+
+			int pre = heap.size() - 1
+			int cur = (cur - 1)/2;
+			HeapNode current = heap.get(cur);
+			while (cur > 0 && current.weight > newNode.weight) {
+				heap.set(pre, current);
+				heap.set(cur, newNode);
+
+				pre = cur;
+				cur = (cur - 1)/2;
+				HeapNode current = heap.get(cur);
+			}
+		}
+
+		public Pair<S,T> pop() {
+			Pair<S,T> retVal = new Pair<>(heap.get(0).data, heap.get(0).weight);
+			
 		}
 	}
 	////////////////////////////////////////////////////////////////////////////////
@@ -65,7 +115,7 @@ public class Graph {
 		if (!mPersonIndex.containsKey(p1)) {
 			throw new PersonNotFoundException("Person \"" + p1.name + "\" that goes to \"" + p1.school + "\" not found.");
 		} else if (!mPersonIndex.containsKey(p2)) {
-			throw new PersonNotFoundException("Person \"" + p2.name + "\" that goes to \"" + p2.school + "\" not found.");
+			throw new PersonNotFoundException(p2);
 		}
 
 		mEdgeIndex.put(p1, new PersonNode(p2, (mEdgeIndex.containsKey(p1) ? mEdgeIndex.get(p1) : null)));
@@ -76,6 +126,25 @@ public class Graph {
 		if (mPersonNameIndex.containsKey(name)) {
 			return mPersonNameIndex.get(name);
 		} else return null;
+	}
+
+	List<Person> shortestPath(Person start, Person finish) throws PersonNotFoundException {
+		BSTN<Person,Integer> fringe = new BSTN<>();
+		HashMap<Person,Integer> distances = new HashMap<>();
+		HashMap<Person,Person> prevPerson = new HashMap<>();
+
+		if (!mPersonIndex.containsKey(start)) throw new PersonNotFoundException(start);
+		if (!mPersonIndex.containsKey(finish)) throw new PersonNotFoundException(finish);
+
+		distances.put(start, 0);
+		for (PersonNode p = mEdgeIndex.get(start); p != null; p = p.next) {
+			distances.put(p.data, 1);
+			fringe.insertNode(p.data, 1);
+		}
+
+		while (!fringe.empty()) {
+
+		}
 	}
 
 	public void printConnections() {
